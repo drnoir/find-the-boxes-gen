0deb452fe40e8997133da27c2a62bc1edf60a23d
chris <chrispgodber@gmail.com>
2021-02-05 11:02:27 +0300

    - added sound - updated game logic - added start button - increased randomness

--- generateBoxes.js
+++ generateBoxes.js
@@ -1,12 +1,21 @@
 // script for generating buildings / game logic etc
-let amountofWinBoxes = 5;
+const buildingsNumber = getRandomInt(200);
+let amountofWinBoxes = Math.floor(buildingsNumber/4);
+
 
 window.onload = function() {
-    let time = 0;
-    createCubes(getRandomInt(200));
-    createWinBoxes(5);
-    updateGameState(time);
-    console.log("loaded");
+
+    const playBtn = document.getElementById("playBtn");
+    const Win = document.getElementById("Win");
+
+    playBtn.addEventListener('click',beginGame);
+    // createCubes(buildingsNumber);
+    // cubesCreated.innerHTML=buildingsNumber.toString();
+    // cubesTotal.innerHTML=amountofWinBoxes.toString();
+    // createWinBoxes(amountofWinBoxes);
+    // updateGameState(time);
+    // document.getElementById('myAudio').play();
+    // console.log("loaded");
 };
 
 AFRAME.registerComponent('player', {
@@ -29,6 +38,7 @@ AFRAME.registerComponent('winbox', {
                 const box = document.querySelector('a-box');
                 let winboxRemove = e.detail.target.el;
                 box.parentNode.removeChild(winboxRemove);
+                document.getElementById('pickup').play();
                 decrementScore();
             }
             e.detail.target.el; // Original entity (playerEl).
@@ -39,31 +49,48 @@ AFRAME.registerComponent('winbox', {
     }
 })
 
+function beginGame(){
+    let time = 0;
+    const timeLeft = document.getElementById('timeLeft');
+    const cubesCreated = document.getElementById('cubesCreated');
+    const cubesLeft= document.getElementById('cubesLeft');
+    const cubesTotal= document.getElementById('totalCubes');
+
+    createCubes(buildingsNumber);
+    cubesCreated.innerHTML=buildingsNumber.toString();
+    cubesTotal.innerHTML=amountofWinBoxes.toString();
+    createWinBoxes(amountofWinBoxes);
+    updateGameState(time);
+    document.getElementById('myAudio').play();
+    document.getElementById('myAudio').volume = 0.5;
+    console.log("Game Started");
+    playBtn.style.visibility = "hidden";
+}
+
 function restart(){
     location.reload();
 }
 
 function decrementScore(){
     amountofWinBoxes--;
-};
+}
 
 function  updateGameState(time){
     setInterval(function(){
         time++;
+        timeLeft.innerHTML=time;
+        cubesLeft.innerHTML=amountofWinBoxes;
         console.log(time, amountofWinBoxes);
         if (time===100){
             restart();
         }
         if (amountofWinBoxes===0){
-            alert("you found all the boxes");
+            document.getElementById("Win").style.visibility = "visible";
+            console.log("Win Condition met");
         }
-
-
 
     }, 1000);
 }
-
-
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
@@ -104,6 +131,7 @@ function createCubes(amount) {
         let posz =getRandomInt(100);
         let scale = getRandomInt(15);
         let colorArr = ['red', 'green', 'blue', 'brown'];
+        let textureArr = []
         let color = getRandomColor(colorArr);
         building.setAttribute('position', {x: posx, y: 10, z: posz});
         building.object3D.scale.set(scale, scale, scale);
